0697c4c1640b61c6f3910fa795a66f6d372ed02b
Sebastian Rodriguez <sebasrm2000@gmail.com>
2022-12-02 17:54:27 -0500

    listo el pollo

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 const menuemail = document.querySelector(".navbar-email");
 const desktopmenu = document.querySelector(".desktop-menu");
 const menucarritoicon = document.querySelector(".navbar-shopping-cart");
+const productDetailCloseIcon = document.querySelector(".product-detail-close")
+
 const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const menuicon = document.querySelector(".menu");
 const mobilemenu = document.querySelector(".mobile-menu");
 const cardsContainer = document.querySelector(".cards-container")
 
-const productDetailContainer = document.querySelector(".product-details")
+const productDetailContainer = document.querySelector("#productDetail")
 
 menuemail.addEventListener("click", toggledesktopmenu);
 menuicon.addEventListener("click", togglemobilemenu);
 menucarritoicon.addEventListener(`click`, togglecarritoaside);
-
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside );
 
 function toggledesktopmenu() { 
 
@@ -31,6 +33,8 @@ function togglemobilemenu(){
     if(!isasideclosed){
         shoppingCartContainer.classList.add(`inactive`);
        }
+
+       closeProductDetailAside();
          
     mobilemenu.classList.toggle(`inactive`);
 }
@@ -44,14 +48,30 @@ function togglecarritoaside(){
    if(!ismobilemenuclosed){
     mobilemenu.classList.add(`inactive`);
    }
+   
+   const isProductDetailClosed= productDetailContainer.classList.contains(`inactive`);
+   
+   if(!isProductDetailClosed){
+    productDetailContainer.classList.toggle(`inactive`);
+   }
+   
+
 
    shoppingCartContainer.classList.toggle(`inactive`)
 
  }
 
- function openProductDetailAside(){
+function openProductDetailAside(){
+   shoppingCartContainer.classList.add('inactive');
     productDetailContainer.classList.remove('inactive');
  }
+
+ function closeProductDetailAside() {
+
+    productDetailContainer.classList.add('inactive');  
+ }
+
+
 
  const productList = [];
  productList.push({
@@ -59,14 +79,14 @@ function togglecarritoaside(){
     price: 120,
     image: `https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`
 
- })
+})
 
  productList.push({
     name: `Pantalla`,
     price: 220,
     image: `https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`
 
- })
+})
 
  productList.push({
     name: `pc`,
@@ -94,10 +114,11 @@ function togglecarritoaside(){
         productcard.classList.add(`product-card`);
     
         //product = {name, price , image}->product.image 
-        const productImg = document.createElement(`Img`);
+        const productImg = document.createElement(`img`);
         productImg.setAttribute(`src`, product.image);
-        productImg.addEventListener('click', openProductDetailAside); 
-    
+        productImg.addEventListener('click', openProductDetailAside )
+
+     
          
         const productinfo = document.createElement(`div`);
         productinfo.classList.add(`product-info`);
